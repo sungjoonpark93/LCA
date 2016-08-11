ae0e99525f0c4eded5d2b19d98b622e35369103e
simpleisthebest <sungjoonpark@korea.ac.kr>
2016-08-11 10:49:12 +0900

    commit

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ import inference
 
 inputfile =""
 
+
 for drug in drugs:
+    dot_list = []
     for time_point in time_points:
         inference_network = inference(time_point_data, algorithm='')
         priorknowledge_edges = search_priorknowledge_edges(inference_network)
-        visualize(inference_network,priorknowledge_edges,is_priorknowledge=False)
+        dot_list.append(get_dot(inference_network,priorknowledge_edges,is_priorknowledge=False))
+
+    postprocessing(dot_list)
