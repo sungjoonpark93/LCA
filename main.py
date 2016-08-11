b1b5519250639a96b45653054e1e5fe0740b09ea
simpleisthebest <sungjoonpark@korea.ac.kr>
2016-08-11 10:37:43 +0900

    commit

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 __author__ = 'SungJoonPark'
+import pandas as pd
+import inference
+
+inputfile =""
+
+for drug in drugs:
+    for time_point in time_points:
+        inference_network = inference(time_point_data, algorithm='')
+        priorknowledge_edges = search_priorknowledge_edges(inference_network)
+        visualize(inference_network,priorknowledge_edges,is_priorknowledge=False)
--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 __author__ = 'SungJoonPark'
+
+from graphviz import Digraph
+dot = Digraph(comment='Thre Round Table')
+dot.node('A')
+dot.node('B')
+dot.node('L')
+dot.edges(['AB','AL'])
+dot.edge('B','L')
+print dot.source
+dot.render('test', view=True)
