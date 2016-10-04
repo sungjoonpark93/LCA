12616832dc0f84442b922eb6b8f5c21ae180130f
simpleisthebest <sungjoonpark@korea.ac.kr>
2016-10-04 17:16:36 +0900

    commit

--- visualization/subprocess_test.py
+++
@@ -1,4 +0,0 @@
-__author__ = 'SungJoonPark'
-from subprocess import check_call
-#check_call(['dot','-Tpng','temp.dot','-o','temp.png'])
-check_call(['neato','temp2.dot','-n','-Tpng','-o','temp2.png'])
